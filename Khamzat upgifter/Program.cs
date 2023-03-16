816627152b0519fb21cdc2f3345ec435a32029b6
Pingvu <nhlpool27@gmail.com>
2023-03-16 13:37:00 +0100

    Add project files.

---
+++ Khamzat upgifter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Övning_1_Khamzat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ålder();
+        }
+
+        static void Ålder()
+        {
+            Console.WriteLine("Vad är din ålder?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age >= 18)
+            {
+                Console.WriteLine("Du är myndig!");
+            }
+            else
+            {
+                Console.WriteLine("Du är inte myndig än.");
+            }
+        }
+    }
+}
